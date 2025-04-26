b2dd0a90d022c4d004a3495195f636bf0e4b351e
Akrate Oussama <aoussama@c2r6p2.1337.ma>
2025-04-26 17:50:38 +0100

    shell

--- pars.c
+++ pars.c
@@ -6,7 +6,7 @@
 /*   By: aoussama <aoussama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:55:00 by aoussama          #+#    #+#             */
-/*   Updated: 2025/04/25 17:58:35 by aoussama         ###   ########.fr       */
+/*   Updated: 2025/04/26 17:42:00 by aoussama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,50 @@ void get_pos(char *str,int *i)
         if (str[*i] == c)
             (*i)++;
     }else{
-        while (str[*i] && str[*i] != '\'' && str[*i] != '"' && str[*i] != ' ')
+        while (str[*i] && str[*i] != '\'' && str[*i] != '"' && str[*i] != ' ' && !is_meta(str[*i]))
             (*i)++;
+    }
+}
+char *chr_meta(char *str,int *i)
+{
+    if (str[*i] == '<')
+    {
+        if (str[(*i) + 1] == '<')
+        {
+            if (str[(*i) + 2] == '<')
+                return (write (1,"parse error near `>'\n",21),NULL);
+            (*i) += 2;
+            return (ft_strdup("<<"));
+        }
+        else{
+            (*i)++;
+            return (ft_strdup("<"));
+        }
+    }else if (str[*i] == '>')
+    {
+        if (str[(*i) + 1] == '>')
+        {
+            if (str[(*i) + 2] == '>')
+                return (write (1,"parse error near `<'\n",21),NULL);
+            (*i) += 2;
+            return (ft_strdup(">>"));
+        }
+        else
+        {
+            (*i)++;   
+            return (ft_strdup(">"));
+        }
+    }else if (str[*i] == '|')
+    {
+        (*i)++;
+        return (ft_strdup("|"));
     }
 }
 t_list *split_cmd(char *str)
 {
     int i;
     int start;
+    char *meta;
     t_list *head = NULL;
     if (!str)
         return (NULL);
@@ -43,9 +79,14 @@ t_list *split_cmd(char *str)
         while (str[i] == ' ')
             i++;
         start = i;
-        while (str[i] && str[i] != ' ')
-        {
+        while (str[i] && str[i] != ' ' && !is_meta(str[i]))
             get_pos(str,&i);
+        if (is_meta(str[i]))
+        {
+            meta = chr_meta(str,&i);
+            if (!meta)
+                return (ft_lstclear(&head), NULL);
+            ft_lstadd_back(&head,ft_lstnew(meta)); 
         }
         ft_lstadd_back(&head,ft_lstnew(ft_substr(str,start,i - start)));
     }
@@ -56,6 +97,8 @@ void paring_cmd(char *cmd)
     t_list *args = split_cmd(cmd);
     
     t_list *tmp = args;
+    if(!tmp)
+        write (1,"error",5);
     while (tmp)
     {
         printf("arg: %s\n", (char *)tmp->content);
